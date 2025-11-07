86db830e9b43d1b1c0a3e5c1eb7263d4f8c292a4
Muhammad Umar <muhammad.umar.218980@gmail.com>
2025-11-07 16:30:22 +0500

    Added Leetcode 347 solution

---
+++ Session 11 Tasks/Leetcode 347.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+       unordered_map<int,int> freq;
+        for(int x : nums) 
+            freq[x] += 1;
+
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> minHeap;
+
+        for(auto &p : freq) {
+            minHeap.push({p.second, p.first}); 
+            if(minHeap.size() > k)
+                minHeap.pop();
+        }
+
+        vector<int> ans;
+        while(!minHeap.empty()) {
+            ans.push_back(minHeap.top().second);
+            minHeap.pop();
+        }
+
+        return ans;
+    }
+};
